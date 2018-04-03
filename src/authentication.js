7d1e9ade08f46b20006aa4767a204fa4b4f679ad
flight9 <ealeaf@sohu.com>
2018-04-03 13:38:21 +0800

    Add after hook to bind user with openid

--- src/authentication.js
+++ src/authentication.js
@@ -22,6 +22,29 @@ module.exports = function (app) {
       remove: [
         authentication.hooks.authenticate('jwt')
       ]
+    },
+    after: {
+      // ZM: To bind a user with openid
+      create: async function (context) {
+        //console.log('After create:', context);
+        const users = app.service('users');
+        const openid = context.data.openid;
+        const user = context.params.user;
+        console.log('After create openid:', openid);
+        console.log('After create user:', user);
+
+        // If visit from web or automatic login by jwt, the openid must be undefined
+        if( openid && user && !user.openid) {
+          const upd = await users.update(user._id, {$set: {openid}});
+          console.log('After create upd_user', upd);
+
+          // TODO: Redirecting to Home/Wait page should be done on client side
+        }
+        // TODO: If a user already had a openid, bind will fail and server should throw an error
+        // or other method to inform the frontend.
+
+        return context;
+      }
     }
   });
 };
